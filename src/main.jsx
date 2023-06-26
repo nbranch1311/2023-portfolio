055d52362d0da1cea94d40ff951c8c396de39020
Nicholas Branch <nicholasbranch@nicholass-mbp.myfiosgateway.com>
2023-06-25 22:14:30 -0400

    New portfolio, tailwind is misbehaving

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+import "./main.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </React.StrictMode>
+);
